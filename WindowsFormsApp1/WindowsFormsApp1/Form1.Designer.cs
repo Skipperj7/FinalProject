001bd2a957fc7bb8d4d2d740e241d0d02071860b
Jacob Cunningham <jacob.joc@gmail.com>
2017-12-04 22:34:19 -0800

    Add files via upload
    TestV1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Points = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(171, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(538, 551);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Points
+            // 
+            this.Points.AutoSize = true;
+            this.Points.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Points.Font = new System.Drawing.Font("Segoe Print", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Points.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Points.Location = new System.Drawing.Point(12, 9);
+            this.Points.Name = "Points";
+            this.Points.Size = new System.Drawing.Size(154, 65);
+            this.Points.TabIndex = 1;
+            this.Points.Text = "Points:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(160, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 65);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(858, 625);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Points);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "CookieClickerTestTitle";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Points;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.MouseClick += new MouseEventHandler(Image1_Clicked);
+        }
+        
+        private int picture = 0;
+        private int score = 0;
+        private string endings;
+        private void Image1_Clicked(object sender, MouseEventArgs e) {
+           
+                picture = picture + 1;
+                score = score + 1;
+                if (picture >= 6) {
+                    picture = 0;
+                }
+                endings = "test";
+                label2.Text = "" + score + endings + "";
+                switch (picture)
+                {
+                    case 0:
+                        pictureBox1.Image = Properties.Resources.p11;
+                        break;
+                    case 1:
+                        pictureBox1.Image = Properties.Resources.p61;
+                        break;
+                    case 2:
+                        pictureBox1.Image = Properties.Resources.p21;
+                        break;
+                    case 3:
+                        pictureBox1.Image = Properties.Resources.p31;
+                        break;
+                    case 4:
+                        pictureBox1.Image = Properties.Resources.p41;
+                        break;
+                    case 5:
+                        pictureBox1.Image = Properties.Resources.p51;
+                        break;
+
+                
+                }
+        }
+    }
+}
