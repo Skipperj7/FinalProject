b62baf34655d53909656fede13e69936c086ccc1
Jacob Cunningham <jacob.joc@gmail.com>
2017-12-05 13:58:23 -0800

    Add files via upload
    v2

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Points = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(171, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(538, 551);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Points
+            // 
+            this.Points.AutoSize = true;
+            this.Points.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Points.Font = new System.Drawing.Font("Segoe Print", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Points.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Points.Location = new System.Drawing.Point(12, 9);
+            this.Points.Name = "Points";
+            this.Points.Size = new System.Drawing.Size(154, 65);
+            this.Points.TabIndex = 1;
+            this.Points.Text = "Points:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(160, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 65);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Window;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(731, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Shop";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(751, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Upgrade";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(773, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "$10";
+            this.label3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(858, 625);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Points);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "CookieClickerTestTitle";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Points;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.MouseClick += new MouseEventHandler(Image1_Clicked);
+            button1.MouseClick += new MouseEventHandler(ShopClick);
+            button2.MouseClick += new MouseEventHandler(upgrade);
+        }
+        
+        private int picture = 0;
+        private double score = 999997;
+        private double scoreToDisplay;
+        private string endings;
+        private void Image1_Clicked(object sender, MouseEventArgs e) {
+           
+                picture = picture + 1;
+                score = score + 1;
+            if (score < 1000000) {
+                scoreToDisplay = score;
+                endings = "";
+            }
+            if (1000000 <= score && score < 1000000000) {
+                scoreToDisplay = Math.Round((score / 1000000), 5);
+               
+                endings = "Million";
+            }
+            if (1000000000 <= score && score < 1000000000000) {
+                scoreToDisplay = Math.Round((score / 1000000000), 5);
+               
+                endings = "Billion";
+            }
+                if (picture >= 6) {
+                    picture = 0;
+                }
+                
+                label2.Text = "" + scoreToDisplay + endings + "";
+                switch (picture)
+                {
+                    case 0:
+                        pictureBox1.Image = Properties.Resources.p11;
+                        break;
+                    case 1:
+                        pictureBox1.Image = Properties.Resources.p61;
+                        break;
+                    case 2:
+                        pictureBox1.Image = Properties.Resources.p21;
+                        break;
+                    case 3:
+                        pictureBox1.Image = Properties.Resources.p31;
+                        break;
+                    case 4:
+                        pictureBox1.Image = Properties.Resources.p41;
+                        break;
+                    case 5:
+                        pictureBox1.Image = Properties.Resources.p51;
+                        break;
+
+                
+                }
+        }
+
+        private void ShopClick(object sender, MouseEventArgs e) {
+            pictureBox1.Enabled = false;
+            button2.Visible = true;
+            button2.Enabled = true;
+            label3.Visible = true;
+        }
+        private void upgrade(object sender, MouseEventArgs e) {
+
+        }
+    }
+}
